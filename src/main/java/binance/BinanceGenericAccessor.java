73bdb835244910e789455bfb76c47ce9e184ccaa
Unknown <ruben.s.ruiz@outlook.com>
2018-02-20 21:44:45 -0600

    Actual first commit
    The first one didn't really count

---
+++ src/main/java/binance/BinanceGenericAccessor.java
@@ -0,0 +1,15 @@
+package binance;
+
+/**
+ * This program allows you to gather price data from binance about certain currencies
+ *
+ */
+public class BinanceGenericAccessor {
+
+  public static double getPriceInBTC() {
+
+    return 0.0;
+
+  }
+
+}
